dd2989e3cc278288199aabb51fa651d4f68882ab
Evert Mouw <post@evert.net>
2019-01-16 03:36:34 +0100

    lots of changes
    - using beaufort for wind speed, so requiring windspeed.php
    - mv index.php get.php
    - replaced and added a few weather emoji for the summary
    - event availability is now "free" (don't show as busy)
    - using the "Conditions" to determine precipitation_icon when temp_min>0
    - choice between one weather icon (calculates averages) or multiple emoji using a URL param
    - moonphase icons and short description (using external lib)
    - wind direction as emoji

--- index.php
+++ index.php
@@ -3,13 +3,22 @@
  * @desc Display yr.no weather forecasts with highly customizable Event titles in your Google Calendar, phone or other iCalendar app
  * @since 2015-04-13
  * @author Allan Laal <allan@permanent.ee>
- * @example http://weather.is.not.permanent.ee/?location=Estonia/Harjumaa/Tallinn&filename=weather.ics
+ * @modified Evert Mouw <post@evert.net> (2019-01-09)
+ * @example http://weather.is.not.permanent.ee/?location=Estonia/Harjumaa/Tallinn&usemanyicons=no&filename=weather.ics
  * @link https://github.com/allanlaal/weather-calendar-feed
  */
-$version = '20150410T000000Z'; // modify this when you make changes in the code!
-$location = param('location', 'Estonia/Harjumaa/Tallinn');
+$version = '20190108T000000Z'; // modify this when you make changes in the code!
+$location = param('location', 'Netherlands/Gelderland/Elspeet');
+// use multiple weather emoji in the summary, or just one weather emoji for the average over the day
+// note: first i used a boolean, but for some reason it didn't work... revertint to yes|no string!
+$usemanyicons = param('usemanyicons', 'yes');
 
 require_once('./vendor/yr-php-library/autoload.php');
+require_once('./vendor/php-moon-phase/MoonPhase.php');
+require_once ("circular.php");
+require_once ("compass.php");
+require_once ("beaufort.php");
+
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
 //echo '<pre>';
@@ -22,9 +31,8 @@ $out .= "CALSCALE:GREGORIAN\r\n";
 $out .= "METHOD:PUBLISH\r\n";
 $out .= "URL:https://github.com/allanlaal/weather-calendar-feed\r\n";
 $out .= "X-WR-CALNAME:Weather\r\n";
-$out .= "X-WR-CALDESC:Display yr.no weather forecasts in Google Calendar.\r\n";
+$out .= "X-WR-CALDESC:Display yr.no weather forecasts (usemanyicons=".$usemanyicons.").\r\n";
 $out .= "X-LOTUS-CHARSET:UTF-8\r\n";
-
 
 // cache forecasts:
 $yr = Yr\Yr::create($location, "./tmp");
@@ -33,7 +41,7 @@ foreach($yr->getPeriodicForecasts(strtotime("now -3 days"), strtotime("now +100 
 {
 	$days[$forecast->getFrom()->format('Y-m-d')][$forecast->getFrom()->format('H:i')] = $forecast;
 }
-	
+
 foreach ($days as $date => $day)
 {
 	$out .= "BEGIN:VEVENT\r\n";
@@ -47,19 +55,53 @@ foreach ($days as $date => $day)
 	$out .= "LAST-MODIFIED:$version\r\n";
 	$out .= "SEQUENCE:0\r\n";
 	$out .= "STATUS:CONFIRMED\r\n";
+	$out .= "TRANSP:TRANSPARENT\r\n";
+	$out .= "X-MICROSOFT-CDO-BUSYSTATUS:FREE\r\n";
+	$out .= "X-MICROSOFT-CDO-INTENDEDSTATUS:FREE\r\n";
+	$out .= "X-MICROSOFT-CDO-ALLDAYEVENT:TRUE\r\n";
 	
+	// get the moon phase
+	$MyMoon = new Solaris\MoonPhase(strtotime($date));
+	$moon_icon = $MyMoon->phase_emoji();
+	$moon_desc = 'Moon phase: ' . $MyMoon->phase_name() . '\n';
+	if ( strtotime($date) > $MyMoon->full_moon() ) {
+		$next_full_moon = $MyMoon->next_full_moon();
+	} else {
+		$next_full_moon = $MyMoon->full_moon();
+	}
+	$moon_desc .= 'Next full moon: ' . gmdate('Y-m-d', $next_full_moon) . '\n';
+	unset ($MyMoon);
+
 	$out .= 'DESCRIPTION:';
 	$temp_max = -1337;
 	$temp_min = 1337;
 	$precipitation = 0;
 	$wind_max = -1337;
+	$manyicons = "";
+	$conditions_wholeday = '';
+	$temperature_wholeday = '';
+	$windspeed_wholeday = '';
+	$winddirection_wholeday = '';
 	foreach ($day as $hour => $forecast)
 	{
-		$out .= $hour.'\n';
-		$out .= 'Temperature: '.$forecast->getTemperature().' °C\n';
+		$windspeed = $forecast->getWindSpeed();
+		$windspeed_wholeday .= $windspeed.'#';
+		$beaufort = Windspeed\beaufort($windspeed);
+		$conditions = $forecast->getSymbol();
+		$conditions_wholeday .= $conditions.'#';
+		$temperature = $forecast->getTemperature();
+		$temperature_wholeday .= $temperature.'#';
+		$winddirection = $forecast->getWindDirection();
+		$winddirection_wholeday .= $winddirection.'#';
+		
+		$daypart_icon = weather_icon($temperature,score_weather_conditions($conditions));
+		$manyicons .= $daypart_icon;
+		
+		$out .= $hour.' '.$daypart_icon.'\n';
+		$out .= 'Temperature: '.$temperature.' °C\n';
 		$out .= 'Precipitation: '.$forecast->getPrecipitation().' mm\n';
-		$out .= 'Conditions: '.$forecast->getSymbol().'\n';
-		$out .= 'Wind: '.$forecast->getWindSpeed().' m/s from '.$forecast->getWindDirection().'\n';
+		$out .= 'Conditions: '.$conditions.'\n';
+		$out .= 'Wind: '.$beaufort.' B from '.$winddirection.'\n';
 		$out .= 'Pressure: '.$forecast->getPressure().' hPa\n';
 		$out .= '\n';
 		
@@ -73,48 +115,67 @@ foreach ($days as $date => $day)
 			$temp_min = $forecast->getTemperature();
 		}
 		
-		if ($wind_max < $forecast->getWindSpeed())
-		{
-			$wind_max = $forecast->getWindSpeed();
-		}
+		#if ($wind_max < $forecast->getWindSpeed())
+		#{
+		#	$wind_max = $forecast->getWindSpeed();
+		#}
 		
 		$precipitation += $forecast->getPrecipitation();
 	}
+	$out .= $moon_desc;
 	$out .= "\r\n";
 
 	// my phone can fit 20 chars, my GoogleCalendar 19 chars:
 	//	1234567890123456789
-	//	-22|-22 12☔ 11.2⇶ <- worst case scenario lengthwise
-	$precipitation_icon = '🌣';
-	if ($temp_min > 0)
-	{ // summer weather
-		if ($precipitation > 0)
-		{
-			$precipitation_icon = '☔';
-		}
-		else
-		{
-			$precipitation_icon = '☀';
-		}
-	}
-	else
-	{
-		if ($precipitation > 0)
-		{
-			$precipitation_icon = '❄';
-		}
-		else
-		{
-			$precipitation_icon = '⛄';
-		}
-	}
-			
-	$out .= "SUMMARY:$temp_max|$temp_min{$precipitation_icon}{$precipitation}㎜{$wind_max}⇶\r\n";	
-
+	//	-22|-22 12☔ 11.2⇶ <- worst case scenario lengthwise		
 	
-	$out .= "TRANSP:OPAQUE\r\n";
+	// use multiple weather emoji or create one weather emoji from the average
+	$weather_icon = '⚠️'; // default emoji, this one you should not see...
+	if ($usemanyicons == 'yes')
+	{
+		$weather_icon = $manyicons;
+	} else {
+		$cond_total = 0;
+		$cond_count = 0;
+		$temp_total = 0;
+		
+		foreach ($conditions_all as $cond)
+		{
+			$cond_total += score_weather_conditions($cond);
+			$cond_count += 1;
+		}
+		$temperature_all = explode('#',substr($temperature_wholeday,0,-1));
+		foreach ($temperature_all as $temp)
+		{
+			$temp_total += score_weather_conditions($cond);
+		}
+		$cond_avg = round ($cond_total / $cond_count);
+		$temp_avg = round ($temp_total / $cond_count);
+		$weather_icon = weather_icon($temp_avg,$cond_avg);
+	}
+	
+	// determine the average wind speed and direction
+	$windspeed_all = explode('#',substr($windspeed_wholeday,0,-1));
+	$totalspeed = 0;
+	$countwind = 0;
+	foreach ($windspeed_all as $speed)
+	{
+		$totalspeed += $speed;
+		$countwind += 1;
+	}
+	$windspeed = round ($totalspeed / $countwind);
+	$beaufort = Windspeed\beaufort($windspeed);
+	#$out .= "DEBUG:".$winddirection_wholeday."\r\n";
+	$winddirection_all = explode('#',substr($winddirection_wholeday,0,-1));
+	#$out .= "DEBUG:".json_encode($winddirection_all)."\r\n";
+	array_walk ( $winddirection_all, 'Compass\compass2degrees_inplace' );
+	#$out .= "DEBUG:".json_encode($winddirection_all)."\r\n";
+	$winddirection_avg = Circular\degrees_circular_avg($winddirection_all);
+	$out .= "DEBUG:".$winddirection_avg."\r\n";
+	$windicon = Compass\arrow(Compass\opposite($winddirection_avg));
+
+	$out .= "SUMMARY: {$weather_icon}  {$temp_min}~{$temp_max}℃  {$beaufort}{$windicon}  {$precipitation}mm {$moon_icon}\r\n";	
 	$out .= "END:VEVENT\r\n";
-			
 }
 
 $out .= 'END:VCALENDAR';
@@ -124,7 +185,6 @@ $out .= 'END:VCALENDAR';
 header('Content-type: text/calendar; charset=utf-8');
 header('Content-Disposition: inline; filename='.param('filename'));
 echo $out;
-
 
 
 /**
@@ -151,4 +211,102 @@ function param($name, $default='')
 	}
 	
 	return $out;
+}
+
+/**
+ * @param integer $conditions
+ * @return string 
+ * @desc score the weather conditions
+ */
+function score_weather_conditions($conditions)
+{
+	if (strpos(strtolower($conditions),'sleet') !== false) {
+		$score = 0;
+	}
+	elseif (strpos(strtolower($conditions),'shower') !== false) {
+		$score = 1;
+	}
+	elseif (strpos(strtolower($conditions),'rain') !== false) {
+		$score = 2;
+	}
+	elseif (strpos(strtolower($conditions),'cloudy') !== false) {
+		$score = 3;
+	}
+	elseif (strpos(strtolower($conditions),'fair') !== false) {
+		$score = 4;
+	}
+	elseif (strpos(strtolower($conditions),'sunny') !== false) {
+		$score = 5;
+	}
+	elseif (strpos(strtolower($conditions),'clear sky') !== false) {
+		$score = 5;
+	}
+	else {
+		$score = 9999;
+	}
+	return $score;
+}
+
+/**
+ * @param integer $temperature
+ * @param integer $condition_score
+ * @return string 
+ * @desc get a weather emoji
+ * a few alternative icons: ☀️  🌞  🌦️ 🌥️ ⛅ 🌤️ 🌩️ ⛈️
+ */
+function weather_icon($temperature,$condition_score)
+{
+	if ($temperature > -1)
+	{
+		switch ($condition_score)
+		{
+			case 0:
+				$weather_icon = '🌨️';
+				break;
+			case 1:
+				$weather_icon = '🚿';
+				break;
+			case 2:
+				$weather_icon = '☔';
+				break;
+			case 3:
+				$weather_icon = '☁️';
+				break;
+			case 4:
+				$weather_icon = '⛅';;
+				break;
+			case 5:
+				$weather_icon = '😎';
+				break;
+			default:
+				$weather_icon = '❓';
+				break;
+		}
+	} else {
+		switch ($condition_score)
+		{
+			case 0:
+				$weather_icon = '🌨️';
+				break;
+			case 1:
+				$weather_icon = '❄️';
+				break;
+			case 2:
+				$weather_icon = '❄️';
+				break;
+			case 3:
+				$weather_icon = '⛄️';
+				break;
+			case 4:
+				$weather_icon = '⛄';;
+				break;
+			case 5:
+				$weather_icon = '⛄';
+				break;
+			default:
+				$weather_icon = '❓';
+				break;
+		}
+	}
+	return $weather_icon;
 }
